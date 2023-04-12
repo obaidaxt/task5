5b2b620da4ae49e3cde99dbc53b20097fb57e91e
obaidaxt <obaidaxt@gmail.com>
2023-04-12 15:20:29 +0200

    Update task5.5.dart

--- task5.5.dart
+++ task5.5.dart
@@ -12,3 +12,30 @@
 //     'Club': 'Al Ahly SC'
 //   }
 // };
+class Player {
+  final String name;
+  final Address address;
+  final List<int> kitNumbers;
+  final String club;
+
+  Player({
+    this.name,
+    this.address,
+    this.kitNumbers,
+    this.club,
+  });
+}
+
+class Address {
+  final String streetName;
+  final String city;
+  final String area;
+  final String country;
+
+  Address({
+     this.streetName,
+     this.city,
+     this.area,
+     this.country,
+  });
+}
